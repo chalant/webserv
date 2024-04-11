f1611b890ec8366bed1fcaba694cc290891993d0
llapage <llapage@student.s19.be>
2024-04-11 11:32:23 +0200

    Router.hpp

--- includes/Router.hpp
+++ includes/Router.hpp
@@ -2,3 +2,46 @@
 in the 'Request' (each locationblock in the conf file corresponds 
 to a 'RequestHandler' mapping, ie the Router selects the correct 
 locationblock)*/
+
+// Router class using std::map for route storage
+
+#ifndef ROUTER_HPP
+#define ROUTER_HPP
+
+#include <string>
+#include <map>
+
+// Forward declarations
+class Request;
+class Response;
+
+// Enum for HTTP methods
+enum HttpMethod {
+    GET = 0,
+    POST = 1,
+    PUT = 2,
+    DELETE = 3,
+};
+
+// Route structure with function pointer for handler
+struct Route {
+public:
+    std::string path;
+    void (*handler)(const Request&, Response&);
+    HttpMethod method;
+
+    // Static path matching function
+    static bool matchPath(const std::string& path, const std::string& pattern);
+};
+
+// Router class using std::map for route storage
+class Router {
+public:
+    void addRoute(const Route& route);
+    Route* findRoute(const Request& request);
+
+private:
+    std::map<std::string, Route> routes_;
+};
+
+#endif
