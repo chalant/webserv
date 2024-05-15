9a893150420d80a86fb24cf801b30f6875b65344
Yves Chalant <ychalant@student.s19.be>
2024-05-15 18:46:15 +0200

    added DefaultConfigurationBlock

--- includes/configuration/DefaultConfigurationBlock.hpp
+++ includes/configuration/DefaultConfigurationBlock.hpp
@@ -1,6 +1,24 @@
 #if !defined(DEFAULTCONFIGURATIONBLOCK_HPP)
 #define DEFAULT_CONFIGURATION_HPP
 
+#include "../../includes/configuration/IConfiguration.hpp"
+
+class DefaultConfigurationBlock: public IConfiguration
+{
+private:
+	/* data */
+public:
+	DefaultConfigurationBlock(/* args */);
+	~DefaultConfigurationBlock();
+};
+
+DefaultConfigurationBlock::DefaultConfigurationBlock(/* args */)
+{
+}
+
+DefaultConfigurationBlock::~DefaultConfigurationBlock()
+{
+}
 
 
 #endif // DEFAULT_CONFIGURATION
--- includes/configuration/Directives.hpp
+++ includes/configuration/Directives.hpp
@@ -2,10 +2,10 @@
 #define DIRECTIVES_HPP
 
 #include "../../includes/utils/Pair.hpp";
-#include "../../includes/configuration/ConfigurationBlock";
+#include "../../includes/configuration/DefaultConfigurationBlock";
 
 #define CLIENT_HEADER_BUFFER_SIZE	Pair<std::string, int>("client_header_buffer_size", 1024);
 #define CLIENT_MAX_URI_SIZE			Pair<std::string, int>("client_max_uri_size", 1024);
-#define HTTP_BLOCK					Pair<std::string, ConfigurationBlock>("http", ConfigurationBlock());
+#define HTTP_BLOCK					Pair<std::string, ConfigurationBlock>("http", DefaultConfigurationBlock());
 
 #endif // DIRECTIVES_HPP
