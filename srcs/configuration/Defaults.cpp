2e910f3baf3dd1e3568b01973c3185d89d8f0e33
Maarten De Cleene <mde-clee@fu-r5-p7.s19.be>
2024-05-21 15:54:04 +0200

    Added a few extra default values and a sample site for testing

--- srcs/configuration/Defaults.cpp
+++ srcs/configuration/Defaults.cpp
@@ -6,18 +6,16 @@ Defaults::Defaults()
     m_directive_parameters[ "client_max_uri_size" ].push_back("1024");
     m_directive_parameters[ "client_body_buffer_size" ].push_back("1024");
     m_directive_parameters[ "client_max_body_size" ].push_back("1024");
-    // //m_directive_parameters[ "prefix" ] =
-    //     "/"; // temporary, the parser should detect the type of prefix.
     m_directive_parameters[ "listen" ].push_back("127.0.0.1:8080");
     m_directive_parameters[ "server_name" ].push_back("default");
-
-    m_directive_parameters[ "error_log" ].push_back("/"); // default path ?
+    m_directive_parameters[ "error_log" ].push_back("/logs/error.log");
     m_directive_parameters[ "error_log" ].push_back("debug");
-
-    m_directive_parameters[ "deny " ].push_back("all");
-
-    m_block_parameters[ "location " ].push_back("/");
+    m_directive_parameters[ "access_log" ].push_back("/logs/access.log");
+    m_directive_parameters[ "deny" ].push_back("all");
+    m_block_parameters[ "location" ].push_back("/");
     m_block_parameters[ "limit_except" ].push_back("GET");
+    m_block_parameters[ "root" ].push_back("sample_site");
+    m_block_parameters[ "index" ].push_back("index.html");
 }
 
 Defaults::~Defaults() {}
