a54458beaf773893e5e7c9bdd0333526e7dfeb56
chalant <chalantvsy@gmail.com>
2024-06-03 11:49:40 +0200

    code cleanup

--- srcs/configuration/ConfigurationLoader.cpp
+++ srcs/configuration/ConfigurationLoader.cpp
@@ -26,14 +26,12 @@ ConfigurationLoader::ConfigurationLoader(ILogger &logger) : m_logger(logger)
     m_separators.push_back("\n");
     m_separators.push_back("\t");
 
-    m_reserved_symbols.push_back("#");
     m_reserved_symbols.push_back("{");
     m_reserved_symbols.push_back("}");
     m_reserved_symbols.push_back(";");
     m_reserved_symbols.push_back("~");
 
-    m_config = NULL;
-    // Log the creation of the configuration loader.
+    m_config = new ConfigurationBlock(m_logger, "main", m_defaults);
     m_logger.log(VERBOSE, "ConfigurationLoader created.");
 }
 
@@ -119,9 +117,6 @@ void ConfigurationLoader::m_addDirective(const Grammar &grammar,
     // the first sub-child is the directive name and the second is the
     // parameters list.
     std::vector<std::string> &params = block.addDirective(directive);
-    // todo: check if the direction is an include, if so, run a configuration
-    // loader and add sub-blocks...
-    //  the first parameter should be a path.
     get_values(tokens, *parse_tree[ 1 ], params);
 }
 
@@ -151,9 +146,9 @@ void ConfigurationLoader::m_buildConfig(const Grammar &grammar,
 
 IConfiguration &ConfigurationLoader::loadConfiguration(const std::string &path)
 {
-    // std::ifstream conf_stream(path.c_str());
-    // if (!conf_stream.is_open())
-    //     throw InvalidConfigFileError();
+    std::ifstream conf_stream(path.c_str());
+    if (!conf_stream.is_open())
+        throw InvalidConfigFileError();
 
     // Log the loading of the configuration file.
     m_logger.log(INFO, "Loading configuration file: '" + path + "'.");
@@ -168,30 +163,27 @@ IConfiguration &ConfigurationLoader::loadConfiguration(const std::string &path)
     NonTerminalSymbol configuration("configuration", 0);
     NonTerminalSymbol block_element("configuration-element", 1);
     NonTerminalSymbol block("block", 2);
-    NonTerminalSymbol block_parameters("block-parameters", 22);
-    NonTerminalSymbol directive("directive", 3);
-    NonTerminalSymbol parameters("parameters", 4);
-    NonTerminalSymbol comment_list("comment-list", 23);
-    NonTerminalSymbol comment("comment", 6);
-    NonTerminalSymbol value("value", 7);
-    NonTerminalSymbol directive_name("directive-name", 8);
-    // NonTerminalSymbol identifier("identifier", 9);
+    NonTerminalSymbol block_parameters("block-parameters", 3);
+    NonTerminalSymbol directive("directive", 4);
+    NonTerminalSymbol parameters("parameters", 5);
+    NonTerminalSymbol comment_list("comment-list", 6);
+    NonTerminalSymbol comment("comment", 7);
+    NonTerminalSymbol value("value", 8);
+    NonTerminalSymbol directive_name("directive-name", 9);
     NonTerminalSymbol text("text", 10);
 
     TerminalSymbol curl_open("{", 11);
     TerminalSymbol curl_close("}", 12);
     TerminalSymbol semicolon(";", 13);
-    TerminalSymbol hashtag("#", 14);
-    TerminalSymbol newline("\n", 15);
-    TerminalSymbol regex("~", 16);
+    TerminalSymbol regex("~", 14);
     std::vector<std::string> bools;
     bools.push_back("on");
     bools.push_back("off");
     std::vector<std::string> empty;
-    TerminalSymbolSet boolean("boolean", 17, bools, substring_matching);
-    TerminalSymbolSet letters("letters", 19, empty, alpha_matching);
-    TerminalSymbolSet digits("digits", 20, empty, digit_matching);
-    TerminalSymbolSet string_("string", 21, empty, printable_ascii_matching);
+    TerminalSymbolSet boolean("boolean", 15, bools, substring_matching);
+    TerminalSymbolSet letters("letters", 16, empty, alpha_matching);
+    TerminalSymbolSet digits("digits", 17, empty, digit_matching);
+    TerminalSymbolSet string_("string", 18, empty, printable_ascii_matching);
 
     // configuration
     GrammarRule *rule = grammar.addRule(configuration);
@@ -205,8 +197,6 @@ IConfiguration &ConfigurationLoader::loadConfiguration(const std::string &path)
     rule->addSymbol(&directive);
     rule = grammar.addRule(block_element);
     rule->addSymbol(&block);
-    rule = grammar.addRule(block_element);
-    rule->addSymbol(&comment_list);
 
     // directive
     rule = grammar.addRule(directive);
@@ -235,32 +225,6 @@ IConfiguration &ConfigurationLoader::loadConfiguration(const std::string &path)
     rule = grammar.addRule(block_parameters);
     rule->addSymbol(&parameters);
 
-    // comment-list
-    rule = grammar.addRule(comment_list);
-    rule->addSymbol(&comment);
-    rule = grammar.addRule(comment_list);
-    rule->addSymbol(&comment);
-    rule->addSymbol(&comment_list);
-
-    // comment
-    rule = grammar.addRule(comment);
-    rule->addSymbol(&hashtag);
-    rule->addSymbol(&text);
-    rule->addSymbol(&semicolon);
-    rule = grammar.addRule(comment);
-    rule->addSymbol(&hashtag);
-    rule->addSymbol(&text);
-    rule = grammar.addRule(comment);
-    rule->addSymbol(&hashtag);
-    rule->addSymbol(&text);
-    rule->addSymbol(&curl_open);
-    rule = grammar.addRule(comment);
-    rule->addSymbol(&hashtag);
-    rule->addSymbol(&text);
-    rule->addSymbol(&curl_close);
-    rule = grammar.addRule(comment);
-    rule->addSymbol(&hashtag);
-
     // parameters
     rule = grammar.addRule(parameters);
     rule->addSymbol(&value);
@@ -278,15 +242,6 @@ IConfiguration &ConfigurationLoader::loadConfiguration(const std::string &path)
     rule = grammar.addRule(directive_name);
     rule->addSymbol(&string_);
 
-    // // identifier
-    // rule = grammar.addRule(identifier);
-    // rule->addSymbol(&letters);
-    // rule = grammar.addRule(identifier);
-    // rule->addSymbol(&underscore);
-    // rule = grammar.addRule(identifier);
-    // rule->addSymbol(&letters);
-    // rule->addSymbol(&identifier);
-
     // text
     rule = grammar.addRule(text);
     rule->addSymbol(&string_);
@@ -294,15 +249,11 @@ IConfiguration &ConfigurationLoader::loadConfiguration(const std::string &path)
     rule->addSymbol(&string_);
     rule->addSymbol(&text);
 
-    // ConfigurationParser	parser;
-    // Pair<const std::vector<Token>&, ParseTree&>	result =
-    // parser.parse(path.c_str(), grammar);
-    std::ifstream conf_stream(path.c_str());
-    if (!conf_stream.is_open())
-        throw InvalidConfigFileError();
     Tokenizer tokenizer(m_separators, m_reserved_symbols);
     Parser parser(grammar);
     const std::vector<Token> &tokens = tokenizer.tokenize(conf_stream);
+	if (tokens.size() == 0)
+		return *m_config;
     ParseTree &parse_tree = parser.parse(tokens);
     if (m_config)
         delete m_config;
